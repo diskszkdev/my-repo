f6c0cb6d27515fb7e1579c00cd59871af9db1a0d
diskszkdev <diskszkdev@gmail.com>
2024-12-26 16:23:02 +0900

    add

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -8,6 +8,7 @@ public class UnitTest1
     [TestMethod]
     public void TestMethod1()
     {
+        var d = DateTime.Now;
         var c = new Class1();
         Assert.AreEqual("Hello World!", c.GetHelloWorld());
     }
