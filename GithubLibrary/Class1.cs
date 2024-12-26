03465311d0acb95c16e1dff1a5c9b343b6dca47a
diskszkdev <diskszkdev@gmail.com>
2024-12-26 16:05:12 +0900

    add project

---
+++ GithubLibrary/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace GithubLibrary;
+
+public class Class1
+{
+    public string GetHelloWorld()
+    {
+        return "Hello World!";
+    }
+    
+    public int Sum(int a, int b)
+    {
+        return a + b;
+    }
+}
---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,21 @@
+using GithubLibrary;
+
+namespace TestProject;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        var c = new Class1();
+        Assert.AreEqual("Hello World!!", c.GetHelloWorld());
+    }
+    
+    [TestMethod]
+    public void TestMethod2()
+    {
+        var c = new Class1();
+        Assert.AreEqual(3, c.Sum(1, 2));
+    }
+}
